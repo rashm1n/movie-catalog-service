f76d97946de89a2582887eb8c0681a58151f5cce
rashm1n <rashmin.ravindu@gmail.com>
2022-02-16 14:33:40 +0530

    Adding Reactive WebClient Support

--- src/main/java/com/rashm1n/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/rashm1n/moviecatalogservice/resources/MovieCatalogResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,7 +20,12 @@ import java.util.stream.Collectors;
 public class MovieCatalogResource {
 
     @Autowired
-    RestTemplate restTemplate;
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+
 
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
@@ -27,6 +33,14 @@ public class MovieCatalogResource {
         List<Rating> ratings = Arrays.asList(new Rating("1234",4), new Rating("3456",6));
 
         return ratings.stream().map(rating -> {
+
+//            webClientBuilder.build()
+//                    .get()
+//                    .uri("http://localhost:8082/movies/"+rating.getMovieId())
+//                    .retrieve()
+//                    .bodyToMono(Movie.class)
+//                    .block();
+
             Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
             return new CatalogItem(movie.getName(),"Test",rating.getRating());
         }).collect(Collectors.toList());
